ee1452c363fde053e78d2aa1c31830e847d9f459
Joe 90 <ee3ojg@gmail.com>
2022-09-27 23:20:38 +0100

    Updated version

--- src/Initialization.cs
+++ src/Initialization.cs
@@ -2,7 +2,7 @@
 
 namespace Oxide.Plugins
 {
-    [Info("Sync Pipes", "Joe 90", "0.9.33")]
+    [Info("Sync Pipes", "Joe 90", "0.9.34")]
     [Description("Allows players to transfer items between containers. All pipes from a container are used synchronously to enable advanced sorting and splitting.")]
     public partial class SyncPipesDevelopment : RustPlugin
     {
